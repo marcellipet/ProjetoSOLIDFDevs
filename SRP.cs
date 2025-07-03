117cb638b57cbc7ce8b1bb082c5b70997593a05b
marcellipet <marcellipetranel@gmail.com>
2025-07-03 13:52:58 -0300

    Adicionar arquivos de projeto.

---
+++ SRP.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Estrutura inicial demonstrando os princípios SOLID
+
+// SRP (Single Responsibility Principle)
+// Violação:
+namespace SOLID.SRP.Violacao.Models
+{
+    public class Funcionario
+    {
+        public void CalcularSalario() { /* cálculo */ }
+        public void GerarRelatorioPDF() { /* violação - responsabilidade a mais */ }
+    }
+}
+
+// Solução:
+namespace SOLID.SRP.Solucao.Models
+{
+    public class Funcionario
+    {
+        public void CalcularSalario() { }
+    }
+
+    public class GeradorRelatorio
+    {
+        public void GerarPDF(Funcionario funcionario) { }
+    }
+}
+
+
